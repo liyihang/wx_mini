7f49fc60cc7218903d81bb76f5700eccbdb0e36f
liyihang <lidoudoou@gmail.com>
2018-11-09 02:09:57 +0800

    layout

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,54 +1,28 @@
-//index.js
-//获取应用实例
-const app = getApp()
-
 Page({
   data: {
-    motto: 'lidoudou',
-    userInfo: {},
-    hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo')
-  },
-  //事件处理函数
-  bindViewTap: function() {
-    wx.navigateTo({
-      url: '../logs/logs'
-    })
-  },
-  onLoad: function () {
-    if (app.globalData.userInfo) {
-      this.setData({
-        userInfo: app.globalData.userInfo,
-        hasUserInfo: true
-      })
-    } else if (this.data.canIUse){
-      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-      // 所以此处加入 callback 以防止这种情况
-      app.userInfoReadyCallback = res => {
-        this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
-        })
-      }
-    } else {
-      // 在没有 open-type=getUserInfo 版本的兼容处理
-      wx.getUserInfo({
-        success: res => {
-          app.globalData.userInfo = res.userInfo
-          this.setData({
-            userInfo: res.userInfo,
-            hasUserInfo: true
-          })
-        }
-      })
-    }
-  },
-  getUserInfo: function(e) {
-    console.log(e)
-    app.globalData.userInfo = e.detail.userInfo
-    this.setData({
-      userInfo: e.detail.userInfo,
-      hasUserInfo: true
-    })
+    imgUrls: [
+      'https://img.alicdn.com/imgextra/i2/1910146537/TB2KskioHZnBKNjSZFrXXaRLFXa_!!1910146537-0-scmitem32000.jpg_2200x2200Q90s50.jpg_.webp',
+      'https://img.alicdn.com/imgextra/i4/1910146537/TB2fcMtoFooBKNjSZFPXXXa2XXa_!!1910146537-0-scmitem32000.jpg_2200x2200Q90s50.jpg_.webp',
+      'https://img.alicdn.com/imgextra/i4/2019459085/TB2Ys1BtpuWBuNjSszbXXcS7FXa_!!2019459085.jpg_2200x2200Q90s50.jpg_.webp'
+    ],
+    menu: [
+      'https://img.alicdn.com/imgextra/i3/2952112410/TB2694Ua1OSBuNjy0FdXXbDnVXa_!!2952112410.jpg_2200x2200Q90s50.jpg_.webp',
+      'https://img.alicdn.com/imgextra/i3/2952112410/TB2694Ua1OSBuNjy0FdXXbDnVXa_!!2952112410.jpg_2200x2200Q90s50.jpg_.webp',
+      'https://img.alicdn.com/imgextra/i3/2952112410/TB2694Ua1OSBuNjy0FdXXbDnVXa_!!2952112410.jpg_2200x2200Q90s50.jpg_.webp',
+      'https://img.alicdn.com/imgextra/i3/2952112410/TB2694Ua1OSBuNjy0FdXXbDnVXa_!!2952112410.jpg_2200x2200Q90s50.jpg_.webp',
+      'https://img.alicdn.com/imgextra/i3/2952112410/TB2694Ua1OSBuNjy0FdXXbDnVXa_!!2952112410.jpg_2200x2200Q90s50.jpg_.webp',
+      'https://img.alicdn.com/imgextra/i3/2952112410/TB2694Ua1OSBuNjy0FdXXbDnVXa_!!2952112410.jpg_2200x2200Q90s50.jpg_.webp',
+      'https://img.alicdn.com/imgextra/i3/2952112410/TB2694Ua1OSBuNjy0FdXXbDnVXa_!!2952112410.jpg_2200x2200Q90s50.jpg_.webp',
+      'https://img.alicdn.com/imgextra/i3/2952112410/TB2694Ua1OSBuNjy0FdXXbDnVXa_!!2952112410.jpg_2200x2200Q90s50.jpg_.webp',
+      'https://img.alicdn.com/imgextra/i3/2952112410/TB2694Ua1OSBuNjy0FdXXbDnVXa_!!2952112410.jpg_2200x2200Q90s50.jpg_.webp',
+
+    ],
+    indicatorDots: true,
+    autoplay: true,
+    interval: 3000,
+    duration: 1000,
+    inputShowed: false,
+    inputVal: ""
   }
+
 })
