5ff163fa128da0e39297d6401f3c00cc41f167d6
liyihang <lidoudoou@gmail.com>
2018-11-17 11:07:23 +0800

    cancel image upload

--- pages/pub/index.js
+++ pages/pub/index.js
@@ -8,6 +8,11 @@ Page({
     src:'https://ss0.bdstatic.com/94oJfD_bAAcT8t7mm9GUKT-xh_/timg?image&quality=100&size=b4000_4000&sec=1542300482&di=c9dceb9380ee0ba603ddbf75e70dafe9&src=http://imgsrc.baidu.com/imgad/pic/item/bd3eb13533fa828b3a7da2e9f71f4134970a5a59.jpg'
   },
 
+  cancelimg(){
+    wx.switchTab({
+      url: '../index/index',
+    })
+  },
   /**
    * 生命周期函数--监听页面加载
    */
